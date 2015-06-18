540f99ca71d41b17beb91500a87d452f9ef0349d
mharpole <mharpole@masonlive.gmu.edu>
2015-06-17 22:03:55 -0400

    Wrote makeCacheMatrix function
    complete intial draft of the makeCacheMatrix function. Fully annotated.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	 #generate a null variable to later store the inverse is
+	i <- null
+	
+	#create a function in the function which can set/reset the matrix
+	set <- function(z){
+				x <<- z
+				i <<= NULL
+		}
+	# create a function which returns the cached matrix
+	get <- function() x
+	
+	# create a function which will set the inverse to i
+	setinverse <- function(solve) i <<-solve
+	
+	# create a function which will return the value stored in i(later will have
+	#the inverse
+	getinverse <- function() i
+	
+	# create a list of the functions embedded within the main function
+	list(set = set, get = get, setinverse = setinverse, 
+	getinverse = getinverse)
 }
 
 
